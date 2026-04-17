ea14fc184c78f8309888e0ffaa0a8b1fff8d2e6f
Gumball04 <Amroshadid@gmail.com>
2026-04-17 21:31:44 +0300

    swagger creat and First batch of DB ORM

---
+++ backend/src/main/java/com/rafeeq/backend/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.rafeeq.backend.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI rafeeqOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("RAFEEQ API")
+                        .description("Backend APIs for Rafeeq Special Education Platform")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("RAFEEQ Team"))
+                        .license(new License()
+                                .name("Private License")));
+    }
+}
